9a665fe0fbcbbde052592fd125a6e3b9299bcda6
Gustavo Martins <gpiccolimartins@gmail.com>
2026-04-09 13:06:25 -0300

    changes

--- src/components/Compras.js
+++ src/components/Compras.js
@@ -430,6 +430,9 @@ const Compras = () => {
               <option value={25}>25</option>
               <option value={50}>50</option>
               <option value={100}>100</option>
+              <option value={250}>250</option>
+              <option value={500}>500</option>
+              <option value={1000}>1000</option>
             </select>
           </div>
           <div className="search-group">
